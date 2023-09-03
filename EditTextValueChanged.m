1c3ff0e460c1fd17a949c8e0238b190ee081752c
Max Twelftree <129371934+maxjtwelftree@users.noreply.github.com>
2023-09-03 16:23:07 +0930

    Add files via upload

---
+++ EditTextValueChanged.m
@@ -0,0 +1,4 @@
+% EDITTEXTVALUECHANGED: Callback for when the value of the EditText field changes
+        function EditTextValueChanged(app, ~)
+            updateTranslationLabel(app); % updates the translationlabel page
+        end
